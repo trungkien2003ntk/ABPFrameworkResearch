0d23bb4e857341230debfde1888a04af51c5abe9
TrungKien <trungkien2003ntk@gmail.com>
2024-07-05 17:47:01 +0700

    feat(BookStore): Add BooksAppService to Application layer

---
+++ DemoProject/aspnet-core/src/MyDemo.Bookstore.Application/Books/BooksAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace MyDemo.Bookstore.Books;
+
+public class BooksAppService : 
+    CrudAppService<
+        Book, 
+        BookDto, 
+        Guid, 
+        PagedAndSortedResultRequestDto, 
+        CreateUpdateBookDto>, 
+    IBooksAppService
+{
+    public BooksAppService(IRepository<Book, Guid> repository)
+        : base(repository)
+    {
+
+    }
+}
