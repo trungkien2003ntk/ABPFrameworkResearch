0c599de15535098a19552d3ede8bd1950fa310c1
TrungKien <trungkien2003ntk@gmail.com>
2024-07-09 16:57:18 +0700

    feat(BookStore) Add Dapper to Execute Stored Procedure

--- DemoProject/aspnet-core/src/MyDemo.Bookstore.Domain.Shared/BookstoreDomainSharedModule.cs
+++ DemoProject/aspnet-core/src/MyDemo.Bookstore.Domain.Shared/BookstoreDomainSharedModule.cs
@@ -1,4 +1,4 @@
-﻿using MyDemo.BookStore.Localization;
+using MyDemo.BookStore.Localization;
 using Volo.Abp.AuditLogging;
 using Volo.Abp.BackgroundJobs;
 using Volo.Abp.FeatureManagement;
@@ -12,6 +12,7 @@ using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
 using Volo.Abp.Validation.Localization;
 using Volo.Abp.VirtualFileSystem;
+using Volo.Abp.Dapper;
 
 namespace MyDemo.BookStore;
 
@@ -25,7 +26,8 @@ namespace MyDemo.BookStore;
     typeof(AbpSettingManagementDomainSharedModule),
     typeof(AbpTenantManagementDomainSharedModule)    
     )]
-public class BookStoreDomainSharedModule : AbpModule
+[DependsOn(typeof(AbpDapperModule))]
+    public class BookStoreDomainSharedModule : AbpModule
 {
     public override void PreConfigureServices(ServiceConfigurationContext context)
     {
